4bcd70e08492ee83ed0b8637274e808eb2c4d29a
nickblank <98602112+nickblank@users.noreply.github.com>
2022-02-04 15:44:27 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int fallCost = 237;
+        int winterCost = 341;
+        int springCost = 254;
+        int summerCost = 172;
+        int totalCost = 0;
+
+        totalCost = fallCost + winterCost + springCost + summerCost;
+        System.out.println("Your total yearly maintenance cost is $" + totalCost);
+    }
+}
